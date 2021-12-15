c00685d88ad56c2cde113e953b1bd5eefa5d21ca
ahlam <khald@MacBook-Air-khald.local>
2021-12-15 09:09:14 +0300

    Photorama2

---
+++ Photorama-Ahlam/Photorama-Ahlam/Model/PhotoDataSource.swift
@@ -0,0 +1,32 @@
+//
+//  PhotoDataSource.swift
+//  Photorama-Ahlam
+//
+//  Created by ahlam  on 09/05/1443 AH.
+//
+
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+  
+  
+  var photos = [Photo]()
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return photos.count
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let identifier = "PhotoCollectionViewCell"
+    let cell =
+    collectionView.dequeueReusableCell(withReuseIdentifier: identifier,
+                                       for: indexPath) as! PhotoCollectionViewCell
+    cell.update(displaying: nil)
+    return cell
+    
+  }
+}
+
+
+
+
