5d362ab828aef048f063fcda79471cfdf03ce8d0
ABRAR ALQARNI <abraralqarni@MacBook-Pro-ABRAR.local>
2021-12-15 10:04:59 +0300

    Photorama-iter2

---
+++ Photorama 21/Photorama/Controller/PhotoInfoViewController.swift
@@ -0,0 +1,38 @@
+//
+//  PhotoInfoViewController.swift
+//  Photorama
+//
+//  Created by ABRAR ALQARNI on 08/05/1443 AH.
+//
+
+import UIKit
+
+class PhotoInfoViewController: UIViewController {
+  
+  @IBOutlet var imageView: UIImageView!
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    store.fetchImage(for: photo) { (result) -> Void in
+      switch result {
+        
+      case let .success(image):
+        self.imageView.image = image
+        
+      case let .failure(error):
+        print("Error fetching image for photo: \(error)")
+      }
+    }
+    
+  }
+  
+  var photo: Photo! {
+    didSet {
+      navigationItem.title = photo.title
+    }
+  }
+  var store: PhotoStore!
+  
+}
