18c3e887d91cff12edec000bfb9ac6e48aacec47
rasha <rasha1418sh@icloud.com>
2021-12-15 09:11:02 +0300

    photorama project

---
+++ Photorama/Photorama/Model/Photo .swift
@@ -0,0 +1,34 @@
+//
+//  Photo .swift
+//  Photorama
+//
+//  Created by rasha  on 08/05/1443 AH.
+//
+
+import Foundation
+
+class Photo : Codable  {
+    let title: String
+//    let remoteURL: URL
+    let remoteURL: URL?
+    let photoID: String
+    let dateTaken: Date
+    
+    
+    
+
+enum CodingKeys: String, CodingKey {
+        case title
+        case remoteURL = "url_z"
+        case photoID = "id"
+        case dateTaken = "datetaken"
+}
+}
+
+
+extension Photo: Equatable {
+    static func == (lhs: Photo, rhs: Photo) -> Bool {
+        // Two Photos are the same if they have the same photoID
+        return lhs.photoID == rhs.photoID
+    }
+}
