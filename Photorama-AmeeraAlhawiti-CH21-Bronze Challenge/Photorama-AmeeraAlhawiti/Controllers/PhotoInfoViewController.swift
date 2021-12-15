e4e853f2417242b75dae6122b106dc09eb4a028a
Ameera BA <alqraniameera@outlook.com>
2021-12-15 10:20:31 +0300

    Finished

---
+++ Photorama-AmeeraAlhawiti-CH21-Bronze Challenge/Photorama-AmeeraAlhawiti/Controllers/PhotoInfoViewController.swift
@@ -0,0 +1,33 @@
+//
+//  PhotoInfoViewController.swift
+//  Photorama-AmeeraAlhawiti
+//
+//  Created by Ameera BA on 14/12/2021.
+//
+
+import UIKit
+
+class PhotoInfoViewController: UIViewController {
+  
+  @IBOutlet var imageView: UIImageView!
+  
+  var photo: Photo! {
+    didSet {
+      navigationItem.title = photo.title
+    }
+  }
+  var store: PhotoStore!
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    store.fetchImage(for: photo) { (result) -> Void in
+      switch result {
+      case let .success(image):
+        self.imageView.image = image
+      case let .failure(error):
+        print("Error fetching image for photo: \(error)")
+      }
+    }
+  }
+}
