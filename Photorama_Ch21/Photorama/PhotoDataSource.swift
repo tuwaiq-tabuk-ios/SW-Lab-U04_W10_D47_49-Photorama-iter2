7c405d63f5f610e433dc6d4c43109f72d40d15e0
Shorouq AlAnzi <shorouqalanzi@MacBook-Air-Shorouq.local>
2021-12-18 21:47:04 +0300

    Photorama 21 + Bronze + Sliver

---
+++ Photorama_Ch21/Photorama/PhotoDataSource.swift
@@ -0,0 +1,31 @@
+//
+//  PhotoDataSource.swift
+//  Photorama
+//
+//  Created by Shorouq AlAnzi on 08/05/1443 AH.
+//
+
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+  
+  var photos = [Photo]()
+  
+  func collectionView(_ collectionView: UICollectionView,
+                      numberOfItemsInSection section: Int) -> Int {
+    return photos.count
+  }
+  
+  
+  func collectionView(_ collectionView: UICollectionView,
+                      cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+    let identifier = "PhotoCollectionViewCell"
+    let cell =
+    collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as! PhotoCollectionViewCell
+    
+    cell.update(displaying: nil)
+    return cell
+    
+  }
+}
