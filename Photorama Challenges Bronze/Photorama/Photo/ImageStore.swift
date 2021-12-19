15c5bcbd1dfed4cd6cfc1aef73f13998ba57f3e7
Atheer abdullah <91871760+atheerab@users.noreply.github.com>
2021-12-19 09:04:29 +0300

    completed CH 21 + Challenges

---
+++ Photorama Challenges Bronze/Photorama/Photo/ImageStore.swift
@@ -0,0 +1,64 @@
+//
+//  ImageStore.swift
+//  Photorama
+//
+//  Created by Atheer abdullah on 09/05/1443 AH.
+//
+
+import Foundation
+import UIKit
+
+
+
+class ImageStore {
+    
+    let cache = NSCache<NSString, UIImage>()
+    
+    func setImage(_ image: UIImage, forKey key: String) {
+        cache.setObject(image, forKey: key as NSString)
+        
+        let imagURL = imageURL(for: key)
+        
+        if let data = image.jpegData(compressionQuality: 0.5) {
+            try? data.write(to: imagURL)
+        }
+    }
+    
+    func image(forKey key: String) -> UIImage? {
+        
+        if let existingImage = cache.object(forKey: key as NSString) {
+            return existingImage
+        }
+        
+        let url = imageURL(for: key)
+        
+        guard let imageFromDisk = UIImage(contentsOfFile: url.path) else {
+            return nil
+        }
+        
+        cache.setObject(imageFromDisk, forKey: key as NSString)
+        
+        return imageFromDisk
+    }
+    
+    func deleteImage(forKey key: String) {
+        cache.removeObject(forKey: key as NSString)
+        
+        let url = imageURL(for: key)
+        
+        do {
+            
+            try FileManager.default.removeItem(at: url)
+            
+        } catch {
+            print("Error removing image at \(url.path)")
+        }
+    }
+    
+    func imageURL(for key: String) -> URL {
+        let docDirectories = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let docDirectory = docDirectories.first!
+        
+        return docDirectory.appendingPathComponent(key)
+    }
+}
---
+++ Photorama/Photorama/Photo/PhotoCollectionViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  UICollectionViewCell.swift
+//  Photorama
+//
+//  Created by Atheer abdullah on 08/05/1443 AH.
+//
+
+
+import UIKit
+
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+  
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var spinner: UIActivityIndicatorView!
+
+
+func update(displaying image: UIImage?) {
+    if let imageToDisplay = image {
+      spinner.stopAnimating()
+      imageView.image = imageToDisplay
+    } else {
+      spinner.startAnimating()
+      imageView.image = nil
+    }
+}
+}
