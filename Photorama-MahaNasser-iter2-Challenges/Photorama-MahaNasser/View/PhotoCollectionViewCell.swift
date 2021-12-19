3d30921a5f4909cc3ef35a51796a8965b9744675
Maha S <mahas@Mahas-MacBook-Air.local>
2021-12-19 14:34:41 +0300

    Photorama iter2 + challenges

---
+++ Photorama-MahaNasser-iter2-Challenges/Photorama-MahaNasser/View/PhotoCollectionViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Photorama-MahaNasser
+//
+//  Created by Maha S on 12/12/2021.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+  
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var spinner: UIActivityIndicatorView!
+  
+  
+  func update(displaying image: UIImage?) {
+      if let imageToDisplay = image {
+          spinner.stopAnimating()
+          imageView.image = imageToDisplay
+      } else {
+          spinner.startAnimating()
+          imageView.image = nil
+      }
+  }
+}
+
